8d773121d6f72ee570b42e69aa0215f19e31b0a7
jeonghunpark1 <p500wh@naver.com>
2025-10-17 10:28:59 +0900

    fix 환경변수 수정

--- food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/FoodDeliveryManagingSystemApplication.java
+++ food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/FoodDeliveryManagingSystemApplication.java
@@ -1,5 +1,6 @@
 package com.example.food_delivery_managing_system;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,5 +12,11 @@ public class FoodDeliveryManagingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryManagingSystemApplication.class, args);
 	}
-
+    @PostConstruct
+    public void printEnv() {
+        System.out.println("[ENV] RDS_URL=" + System.getenv("RDS_URL"));
+        System.out.println("[ENV] RDS_USERNAME=" + System.getenv("RDS_USERNAME"));
+        String pw = System.getenv("RDS_PASSWORD");
+        System.out.println("[ENV] RDS_PASSWORD length=" + (pw == null ? "null" : pw.length()));
+    }
 }
