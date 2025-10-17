26b3fdff2bdb9f545fa4a7ae790fe60c19ec6b03
jeonghunpark1 <p500wh@naver.com>
2025-10-17 15:12:57 +0900

    fix 로그인 로그아웃 버튼

--- food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/FoodDeliveryManagingSystemApplication.java
+++ food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/FoodDeliveryManagingSystemApplication.java
@@ -12,11 +12,5 @@ public class FoodDeliveryManagingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryManagingSystemApplication.class, args);
 	}
-    @PostConstruct
-    public void printEnv() {
-        System.out.println("[ENV] RDS_URL=" + System.getenv("RDS_URL"));
-        System.out.println("[ENV] RDS_USERNAME=" + System.getenv("RDS_USERNAME"));
-        String pw = System.getenv("RDS_PASSWORD");
-        System.out.println("[ENV] RDS_PASSWORD length=" + (pw == null ? "null" : pw.length()));
-    }
+
 }
