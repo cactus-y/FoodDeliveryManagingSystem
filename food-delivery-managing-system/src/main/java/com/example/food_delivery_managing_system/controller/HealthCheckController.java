a09619badbda2f775d1d06c353d0e6e243f0f690
jeonghunpark1 <p500wh@naver.com>
2025-10-20 11:46:51 +0900

    fis health 내용 추가

---
+++ food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.example.food_delivery_managing_system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok"; // "ok" 문자열과 함께 HTTP 200 응답을 반환
+    }
+}
