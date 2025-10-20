84fb70287fb8605cd4be34988008aa320a64f334
jeonghunpark1 <p500wh@naver.com>
2025-10-20 14:06:46 +0900

    fix 충돌 문제 해결

--- food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/controller/HealthCheckController.java
+++ food-delivery-managing-system/src/main/java/com/example/food_delivery_managing_system/controller/HealthCheckController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/")
-    public String root() {
-        return "ok"; // "ok" 문자열과 함께 HTTP 200 응답을 반환
-    }
-
     @GetMapping("/health")
     public String healthCheck() {
         return "ok"; // "ok" 문자열과 함께 HTTP 200 응답을 반환
